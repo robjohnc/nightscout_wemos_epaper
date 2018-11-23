7637fa5c0c1ecb56d33956c30117b4c8476cdd5e
robjohnc <robjohnc@gmail.com>
2018-11-23 17:20:04 +0000

    BST Addition
    Adding British summer time correction if applicable

--- Nightscout_wemos_epaper.ino
+++ Nightscout_wemos_epaper.ino
@@ -1,18 +1,18 @@
-
 //load libraries - ESP8266 Wifi, SSL support, JSON parser, Time Libraries, and Adafruit Graphics library for epaper display
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 #include <TimeLib.h>
 #include <Time.h>
+#include <LiquidCrystal_I2C.h>
 #include <GxEPD.h>
 
 //define which epaper diaply we're using
-#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
+#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w e-paper screen
 
 
 
-// load fonts for epaper display
+// FreeFonts from Adafruit_GFX
 #include <Fonts/NotoSansUI9pt7b.h>
 #include <Fonts/NotoSansUI12pt7b.h>
 #include <Fonts/NotoSansUI18pt7b.h>
@@ -23,6 +23,7 @@
 #include <Fonts/FreeSansBold9pt7b.h>
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
+
 
 //set epaper output pins. These should be connected as follows to the D1 Mini:
 //  BUSY -> D2
@@ -39,59 +40,62 @@ GxEPD_Class display(io);
 
 
 
-//wifi connection details
-const char* ssid = "YOUR SSID HERE";
-const char* password = "YOUR WIFI PASSWORD";
+
+
+const char* ssid = "SSID"; //enter your SSID
+const char* password = "password"; //enter your wifi password
 //seconds to sleep for
 const int sleepTimeS = 60;
-//host to connect to
-const char* host = "NIGHSCOUT HOST HERE";
-const int httpsPort = 443;
-//setup JSON Buffer
-const size_t bufferSize = 3*JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(13) + 280;
 
-// SHA1 fingerprint of the host's SSL certificate
-const char* fingerprint = "CE:CE:49:37:75:F4:61:4D:5D:4A:D2:B3:E0:19:9B:FF:EF:E9:A0:76";
-
-//read voltage on pin A0 to get battery voltage through resistor divider
+// this section gets the current voltage (approximately) on pin A0 if it is connected in a resistor voltage divider like this: VCC -> 100K -> A0 -> 330K -> GND
 int raw = analogRead(A0);
 float volt=raw/1023.0;
-float getVcc=volt*4.408;
+float getVcc=raw / 194.2;//*4.408;
+
+
+const char* host = "NIGHTSCOUT HOST"; //enter your nightscout host here - no "https://" needed
+const int httpsPort = 443;
+  const size_t bufferSize = 3*JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(13) + 280;
+// SHA1 fingerprint of the host's SSL certificate
+const char* fingerprint = "CE:CE:49:37:75:F4:61:4D:5D:4A:D2:B3:E0:19:9B:FF:EF:E9:A0:76"; //enter the https fingerprint if you want this verification
+
 
 void setup() {
   
   Serial.begin(115200);
   Serial.setTimeout(2000);
   Serial.println();
+  Serial.println(raw);
+  Serial.println(getVcc);
   display.init();
-  //Serial.print("connecting to ");
-  //Serial.println(ssid);
  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
 
   }
- //print Wifi address to Serial interface
   Serial.println("");
-  Serial.print(WiFi.localIP());
+ 
+  //lcd.print(WiFi.localIP());
 
   // Use WiFiClientSecure class to create TLS connection
   WiFiClientSecure client;
+
   if (!client.connect(host, httpsPort)) {
     Serial.println("connection failed");
     Serial.println("Sleeping...");
-    //sleep if we failed, retry next time
     ESP.deepSleep(sleepTimeS * 1000000);
     return;
   }
-//rudimentary check on Hash of SSL certificate - should match, but we carry on if the fingerprint changes
+
  if (client.verify(fingerprint, host)) {
    Serial.println("certificate matches");
+ 
  } else {
    Serial.println("certificate doesn't match");
+ 
  }
-//get the data at "<host>/pebble"
+
   String url = "/pebble";
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
@@ -101,13 +105,11 @@ void setup() {
   //Serial.println("request sent");
   while (client.connected()) {
     String line = client.readStringUntil('\n');
-    //read until we get the headers, then continue
     if (line == "\r") {
       //Serial.println("headers received");
       break;
     }
   }
-  //first line we don't need
   String line2 = client.readStringUntil('\n');
   Serial.println("==========");
   //read the JSON line into "line2"
@@ -115,65 +117,84 @@ void setup() {
         //parse the JSON into variables
         DynamicJsonBuffer jsonBuffer(bufferSize);
         JsonObject& root = jsonBuffer.parseObject(line2);
-        String status0_now = root["status"][0]["now"]; // 1517649894544
-        JsonObject& bgs0 = root["bgs"][0];
-        const char* bgs0_sgv = bgs0["sgv"]; // "11.0"
-        int bgs0_trend = bgs0["trend"]; // 4
-        const char* bgs0_direction = bgs0["direction"]; // "Flat"
-        String bgs0_datetime = bgs0["datetime"]; // 1517649566004
-        const char* bgs0_battery = bgs0["battery"]; // "100"
-        JsonObject& cals0 = root["cals"][0];
-        float cals0_slope = cals0["slope"]; // 1047.705602117555
-        float cals0_intercept = cals0["intercept"]; // 21408.668786983755
-        int cals0_scale = cals0["scale"]; // 1
-
-//process the time
-            //strip milliseconds from the timestamp
+        
+        String status0_now = root["status"][0]["now"]; 
             status0_now = status0_now.substring(0, status0_now.length() - 3);
-            //make the time an integer instead of a string
             time_t status0_now1 = status0_now.toInt();
-            //strip milliseconds from data timestamp
-            bgs0_datetime = bgs0_datetime.substring(0, bgs0_datetime.length() - 3);
-            //make the data timestamp an integer instead of a string
-            time_t bgs0_datetime3 = bgs0_datetime.toInt();
-            //generate timenow string "xx:xx" to include leading zeroes if needed
-            String timeNow = String(hour(status0_now1)) + ":";
-            if (minute(status0_now1) < 10){timeNow = timeNow + "0";} 
-            timeNow = timeNow + String(minute(status0_now1));
-            //generate dataAge in minutes 
-            int dataAge = (status0_now1 - bgs0_datetime3) / 60;
+        JsonObject& bgs0 = root["bgs"][0];
+        const char* bgs0_sgv = bgs0["sgv"]; 
+        int bgs0_trend = bgs0["trend"]; 
+        const char* bgs0_direction = bgs0["direction"]; 
+            time_t bgs0_datetime = bgs0["datetime"];
+        String bgs0_datetime2 = bgs0["datetime"]; 
+            bgs0_datetime2 = bgs0_datetime2.substring(0, bgs0_datetime2.length() - 3);
+            time_t bgs0_datetime3 = bgs0_datetime2.toInt();
+//check for British summer time and adjust display time accordingly
+            if (isBST(year(status0_now1), month(status0_now1), day(status0_now1), hour(status0_now1))){
+              status0_now1 = status0_now1 +3600;
+              }
+            if (isBST(year(bgs0_datetime3), month(bgs0_datetime3), day(bgs0_datetime3), hour(bgs0_datetime3))){
+              bgs0_datetime3 = bgs0_datetime3 +3600;
+              }
+            
+
+        const char* bgs0_battery = bgs0["battery"]; 
+        JsonObject& cals0 = root["cals"][0];
+        float cals0_slope = cals0["slope"]; 
+        float cals0_intercept = cals0["intercept"]; 
+        int cals0_scale = cals0["scale"]; 
+
         
 Serial.print("Time Now: ");
-Serial.println(timeNow);
+Serial.print(hour(status0_now1));
+Serial.print(":");
+Serial.println(minute(status0_now1));
+String timeNow = String(hour(status0_now1)) + ":";
+if (minute(status0_now1) < 10){timeNow = timeNow + "0";} 
+timeNow = timeNow + String(minute(status0_now1));
+//print current time
 Serial.print("As at: ");
 Serial.print(hour(bgs0_datetime3));
 Serial.print(":");
 Serial.println(minute(bgs0_datetime3));
+int dataAge = (status0_now1 - bgs0_datetime3) / 60;
 Serial.print("Data Age (minutes): ");
 Serial.println(dataAge);
 Serial.print("Blood Glucose is:  ");
 Serial.print(String(bgs0_sgv) + " ");
+char* directarr = "";
+if (String(bgs0_direction) == "Flat") {
+    directarr = "→︎";
+}  else if (String(bgs0_direction) == "FortyFiveUp") {
+    directarr = "↗︎";
+}  else if (String(bgs0_direction) == "FortyFiveDown") {
+    directarr = "↘";
+} else if (String(bgs0_direction) == "DoubleUp") {
+    directarr = "↑↑";
+} else if (String(bgs0_direction) == "DoubleDown") {
+    directarr = "↓↓";
+ }
+Serial.println(directarr);
+
 Serial.print("Battery Voltage is:  ");
 Serial.println(getVcc);
 
-//call a function to display the data on our e-paper display
+//call the "showdata" function to show the data on the LCD screen
 showdata(timeNow,bgs0_sgv,dataAge,getVcc);
 
-Serial.println("Sleeping...");
-//Go to sleep - Pin D0 must be connected to RST
-ESP.deepSleep(sleepTimeS * 1000000);
-  
+
+/sleep for a minute
+  Serial.println("Sleeping...");
+  ESP.deepSleep(sleepTimeS * 1000000);
+
 }
-
-
 void loop() {
-//do nothing - everything is executed only once, in setup
 }
 
 
 void showdata(String timenow, String BG, int age, float volts)
 {
-  //display.fillScreen(GxEPD_WHITE);
+
   display.setTextColor(GxEPD_BLACK);
 
   display.setRotation(1);
@@ -184,7 +205,6 @@ void showdata(String timenow, String BG, int age, float volts)
   display.print("BG:");
   display.setFont(&NotoSansUI_Regular36pt7b);
   display.setCursor(150,100);
-  //display.println("22.4");
   display.println(BG);
   
 
@@ -205,7 +225,53 @@ void showdata(String timenow, String BG, int age, float volts)
   display.setCursor(0, 120);
   display.println(String(volts) + "V");
   
-  //push all this to the display
   display.update();
 
+}
+bool isBST(int year, int month, int day, int hour)
+{
+    // bst begins at 01:00 gmt on the last sunday of march
+    // and ends at 01:00 gmt (02:00 bst) on the last sunday of october
+    // january, february, and november are out
+    if (month < 3 || month > 10) { return false; }
+    // april to september are in
+    if (month > 3 && month < 10) { return true; }
+    // last sunday of march
+    int lastMarSunday =  (31 - (5* year /4 + 4) % 7);
+    // last sunday of october
+    int lastOctSunday = (31 - (5 * year /4 + 1) % 7);
+    // in march we are bst if its past 1am gmt on the last sunday in the month
+    if (month == 3)
+    {
+        if (day > lastMarSunday)
+        {
+            return true;
+        }
+        if (day < lastMarSunday)
+        {
+            return false;
+        }
+        if (hour < 1)
+        {
+            return false;
+        }
+        return true;
+    }
+    // in october we must be before 1am gmt (2am bst) on the last sunday to be bst
+    if (month == 10)
+    {
+        if (day < lastOctSunday)
+        {
+            return true;
+        }
+        if (day > lastOctSunday)
+        {
+            return false;
+        }
+        if (hour >= 1)
+        {
+            return false;
+        }
+        return true;
+    }
 }
